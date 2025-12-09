63c48e1b96768d31f0f518a1110704a094718068
Mohamed Amine Larbi <larbi.mohamedamine@esprit.tn>
2025-12-09 17:55:25 +0100

    initialisation du projet

---
+++ src/main/java/tn/esprit/projetapprentissagespringboot/ProjetApprentissageSpringBootApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.projetapprentissagespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetApprentissageSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetApprentissageSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/esprit/projetapprentissagespringboot/ProjetApprentissageSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.projetapprentissagespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetApprentissageSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
