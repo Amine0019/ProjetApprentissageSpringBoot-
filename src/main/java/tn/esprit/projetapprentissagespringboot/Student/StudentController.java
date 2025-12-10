026886918b10d1791979bb808d43acfda531240a
Mohamed Amine Larbi <larbi.mohamedamine@esprit.tn>
2025-12-10 18:39:37 +0100

    le premier endpoint

---
+++ src/main/java/tn/esprit/projetapprentissagespringboot/Student/StudentController.java
@@ -0,0 +1,20 @@
+package tn.esprit.projetapprentissagespringboot.Student;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/students")
+public class StudentController {
+
+    @GetMapping
+    public List<String> findAllStudents() {
+        return List.of(
+                "Mohamed Amine Larbi",
+                "Kousay Ben Ayed"
+        );
+    }
+}
