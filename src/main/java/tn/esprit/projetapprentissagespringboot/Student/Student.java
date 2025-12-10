6ba4ed3b4ebf49181cb91aa9f94006e0eba4d882
Mohamed Amine Larbi <larbi.mohamedamine@esprit.tn>
2025-12-11 00:23:23 +0100

    update class Student

--- src/main/java/tn/esprit/projetapprentissagespringboot/Student/Student.java
+++ src/main/java/tn/esprit/projetapprentissagespringboot/Student/Student.java
@@ -1,56 +1,33 @@
 package tn.esprit.projetapprentissagespringboot.Student;
 
-import java.time.LocalDate;
+import jakarta.persistence.*;
 
+import java.time.LocalDate;
+@Entity
+@Table(name = "student")
 public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int Id;
 
     private String firstName;
     private String lastName;
     private LocalDate dateOfBirth;
+    @Column(unique = true )
     private String email;
+    @Transient
     private int age;
 
     public Student() {
     }
 
-    public Student(String firstName, String lastName, LocalDate dateOfBirth, String email, int age) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.dateOfBirth = dateOfBirth;
-        this.email = email;
+    public Student(int age, String email, LocalDate dateOfBirth, String lastName, String firstName, int id) {
         this.age = age;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public LocalDate getDateOfBirth() {
-        return dateOfBirth;
-    }
-
-    public void setDateOfBirth(LocalDate dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
         this.email = email;
+        this.dateOfBirth = dateOfBirth;
+        this.lastName = lastName;
+        this.firstName = firstName;
+        Id = id;
     }
 
     public int getAge() {
@@ -61,7 +38,43 @@ public class Student {
         this.age = age;
     }
 
+    public String getEmail() {
+        return email;
+    }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
 
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
 }
